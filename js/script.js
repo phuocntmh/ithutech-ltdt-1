9dd4e33c28c7b8d1c3e214df0795c1bad0f8547b
Nguyễn Trọng Minh Hồng Phước <39150891+phuocntmh@users.noreply.github.com>
2019-05-18 22:01:12 +0700

    ver. 1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var frm = $('#register-form');
+frm.submit(function (e) {
+    var recaptcha = $("#g-recaptcha-response").val();
+    if (recaptcha === "") {
+        e.preventDefault();
+        alert("Vui lòng kiểm tra biểu mẫu. Error: Please check the Recaptcha.");
+    }
+    else {
+        e.preventDefault();
+        $.ajax({
+            type: 'GET',
+            beforeSend: function(){
+                $("input").prop('disabled', true);
+                $("#submit-form").attr("disabled", true);
+            },
+            url: '',
+            data: frm.serialize(),
+            success: function (data) {
+                $('#SuccessModal').modal('show');
+            },
+            error: function (data) {
+                $('#FailModal').modal('show');
+            },
+            complete: function(){
+                $("input").prop('disabled', false);
+                $("#submit-form").attr("disabled", false);
+                $('#register-form')[0].reset();
+            }
+        });
+    }
+});
